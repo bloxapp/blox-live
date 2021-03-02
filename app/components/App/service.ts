a228b460a13061832bd15f2a2a744b6f53394953
Vadim Chumak <vadim.inc@gmail.com>
2021-03-02 16:59:01 +0100

    fix token parser

--- app/components/App/service.ts
+++ app/components/App/service.ts
@@ -11,9 +11,10 @@ export const initApp = () => {
 export const deepLink = (onSuccess, onFailure) => {
   remote.app.on('open-url', (_event, data) => {
     if (data) {
-      const questionMarkIndex = data.indexOf('//');
-      const trimmedCode = data.substring(questionMarkIndex + 2);
-      const params : Record<string, any> = queryString.parse(trimmedCode);
+      // const questionMarkIndex = data.indexOf('//');
+      // const trimmedCode = data.substring(questionMarkIndex + 2);
+      const [, query] = data.split('//');
+      const params : Record<string, any> = queryString.parse(query);
       try {
         if (Object.keys(params).length > 0) {
           onSuccess(params);
@@ -32,10 +33,11 @@ export const deepLink = (onSuccess, onFailure) => {
     logger.trace(commandLine);
     const cmd = commandLine[2] || commandLine[1];
     if (cmd && cmd.includes('blox-live://')) {
-      const questionMarkIndex = cmd.indexOf('//');
-      const trimmedCode = cmd.substring(questionMarkIndex + 2);
-      const withoutSlash = trimmedCode.slice(0, trimmedCode.length - 1);
-      const params : Record<string, any> = queryString.parse(withoutSlash);
+      // const questionMarkIndex = cmd.indexOf('//');
+      // const trimmedCode = cmd.substring(questionMarkIndex + 2);
+      // const withoutSlash = trimmedCode.slice(0, trimmedCode.length - 1);
+      const [, query] = cmd.split('//');
+      const params : Record<string, any> = queryString.parse(query);
       try {
         if (params) {
           const win = remote.getCurrentWindow();
