7e73d71963a57963d94da7629de94595283a7661
guy muroch <guym@blox.io>
2021-08-22 13:22:48 +0300

    fix beaconcha link and change linux and windows keyvault-cli

--- app/backend/common/communication-manager/beaconcha-api.ts
+++ app/backend/common/communication-manager/beaconcha-api.ts
@@ -5,8 +5,16 @@ import Connection from '../store-manager/connection';
 export default class BeaconchaApi extends Http {
   init = (network? : string) => {
     const networkFromStore = Connection.db().get('network');
-    this.instance.defaults.baseURL = (network || networkFromStore) === 'pyrmont'
-      ? config.env.PYRMONT_BEACONCHA_URL
-      : config.env.BEACONCHA_URL;
+    this.instance.defaults.baseURL = this.getBeaconChaByNetwork((network || networkFromStore));
+  };
+  getBeaconChaByNetwork = (network) => {
+    switch (network) {
+      case 'pyrmont':
+        return config.env.PYRMONT_BEACONCHA_URL;
+      case 'prater':
+        return config.env.PRATER_BEACONCHA_URL;
+      default:
+        return config.env.BEACONCHA_URL;
+    }
   };
 }
